3884c6228869fbb439d5b7fa5b8581416ea12288
RachelHyunjiLee <racheljames@naver.com>
2026-07-14 11:47:15 +0900

    Initialize main.jsx with React and App component

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './esg-dashboard.jsx' // 보내주신 파일명과 연결
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App /> 
+  </React.StrictMode>,
+)
